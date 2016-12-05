8247375c050cb001a7c72747a6ee1241f1e7f42b
Denzed <dansmi97@gmail.com>
2016-12-05 10:38:13 +0300

    Restored the control over number of players

--- app/src/main/java/com/cow006/gui/SetupSoloGameActivity.java
+++ app/src/main/java/com/cow006/gui/SetupSoloGameActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 
 public class SetupSoloGameActivity extends AppCompatActivity {
@@ -21,6 +22,8 @@ public class SetupSoloGameActivity extends AppCompatActivity {
 
     public void startGame(View view) {
         Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("Bot count", ((SeekBar) findViewById(R.id.botNumberSeekBar)).getProgress());
+        intent.putExtra("Bot level", ((SeekBar) findViewById(R.id.botLevelSeekBar)).getProgress());
         startActivity(intent);
     }
 }
